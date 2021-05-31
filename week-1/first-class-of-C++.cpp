4e2b6f2596c1c6285211ec6367dcd1e6a6f0f0a5
Lara Souza <80922589+larasouzabr@users.noreply.github.com>
2021-05-31 14:52:00 -0300

    Create first-class-of-C++.cpp

---
+++ week-1/first-class-of-C++.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+int main() {
+
+cout <<"digite um numero para A" <<endl;  
+int a;
+int b;
+cin>>a; // scanf do C
+cout<< "digite um valor para B" <<endl;
+cin>>b; 
+int soma;
+soma = a + b;
+float media;
+media = (float)soma/2;
+cout << fixed << setprecision (2)<< media<<endl;
+}
